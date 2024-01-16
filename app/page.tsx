47146666e3b06d781885c52324ee134330984b87
lilyanb <lilyan.bastien@gmail.com>
2024-01-16 20:50:28 +0100

    init front-end

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import {Button} from "@nextui-org/react";
+
+
+export default function Home() {
+  return (
+    <main className="">
+      <Button className='bg-white rounded-[0.5rem]'>Click me</Button>
+    </main>
+  )
+}
