5bb514349cd155636a4dd2d61776ad4375e5abcb
Nandy Bâ <nandyba@yahoo.fr>
2024-01-16 02:06:29 +0100

    feature: add facillitator

--- test/GhoUpdatedMainnet.t.sol
+++ test/GhoUpdatedMainnet.t.sol
@@ -22,4 +22,16 @@ contract ForkTest is Test {
         ghoToken = GhoToken(address(0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f));
     }
 
+    function test_addFacilitator() public {
+        address manager = address(0x5300A1a15135EA4dc7aD5a167152C01EFc9b192A);
+        address facilitator = address(0x1);
+        string memory label = "test";
+        uint128 bucketCapacity = 100e18;
+        vm.prank(manager);
+        ghoToken.addFacilitator(facilitator, label, bucketCapacity);
+        assertEq(ghoToken.getFacilitator(facilitator).label, label);
+        assertEq(ghoToken.getFacilitator(facilitator).bucketCapacity, bucketCapacity);
+        assertEq(ghoToken.getFacilitator(facilitator).bucketLevel, 0);
+    }
+
 }
