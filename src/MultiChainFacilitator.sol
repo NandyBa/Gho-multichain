80f842c8843191b32dd492ba6cfd4521a02b7934
lilyanb <lilyan.bastien@gmail.com>
2024-01-20 15:14:12 +0100

    add MultiChainFacilitator

---
+++ src/MultiChainFacilitator.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Withdraw} from "./utils/Withdraw.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IGhoToken} from "./interface/IGhoToken.sol";
+
+contract MultiChainFacilitator is Withdraw, CCIPReceiver {
+    address Gho_Address;
+
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+
+    address immutable i_router;
+    address immutable i_link;
+
+    event MessageSent(bytes32 messageId);
+    event MintCallSuccessfull();
+
+    constructor(
+        address router,
+        address link,
+        address _Gho_Address
+    ) CCIPReceiver(router) {
+        Gho_Address = _Gho_Address;
+        i_router = router;
+        i_link = link;
+    }
+
+    receive() external payable {}
+
+    function burn(
+        uint64 destinationChainSelector,
+        address receiver,
+        PayFeesIn payFeesIn,
+        uint256 amount
+    ) external {
+        IGhoToken GHOContract = IGhoToken(Gho_Address);
+        require(
+            GHOContract.transferFrom(msg.sender, address(this), amount),
+            "can't transfert"
+        );
+        uint256 balanceBefore = GHOContract.balanceOf(address(this));
+        GHOContract.burn(amount);
+        uint256 balanceAfter = GHOContract.balanceOf(address(this));
+        require((balanceBefore - balanceAfter) == amount, "can't burn");
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: abi.encodeWithSignature(
+                "mint(address, amount)",
+                msg.sender,
+                amount
+            ),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: payFeesIn == PayFeesIn.LINK ? i_link : address(0)
+        });
+
+        uint256 fee = IRouterClient(i_router).getFee(
+            destinationChainSelector,
+            message
+        );
+
+        bytes32 messageId;
+
+        if (payFeesIn == PayFeesIn.LINK) {
+            LinkTokenInterface(i_link).approve(i_router, fee);
+            messageId = IRouterClient(i_router).ccipSend(
+                destinationChainSelector,
+                message
+            );
+        } else {
+            messageId = IRouterClient(i_router).ccipSend{value: fee}(
+                destinationChainSelector,
+                message
+            );
+        }
+
+        emit MessageSent(messageId);
+    }
+
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+        (bool success, ) = address(Gho_Address).call(message.data);
+        require(success);
+        emit MintCallSuccessfull();
+    }
+}
